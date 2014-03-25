ca032c602f718909ca9b667c1b7eb4fec897cc07
Israel Felipe de Lima Araújo Silva <israelita.felipe@gmail.com>
2014-03-25 17:45:33 -0300

    PROJETO INICIAL

---
+++ GLB/src/main/java/br/com/sglb/model/dao/InterfaceDAO.java
@@ -0,0 +1,24 @@
+package br.com.sglb.model.dao;
+
+import java.io.Serializable;
+import java.util.List;
+import org.hibernate.criterion.DetachedCriteria;
+
+/**
+ *
+ * @author Israel Araújo
+ */
+public interface InterfaceDAO<T> {
+    
+    // METODOS
+    void save (T entity);
+    void update (T entity);
+    void remove (T entity);
+    void merge (T entity);
+    
+    // ENCAPSULAMENTO
+    T getEntity (Serializable id);
+    T getEntityByDetachedCriteria(DetachedCriteria criteria);
+    List<T> getEntities();
+    List<T> getEntitiesByDetachedCriteria(DetachedCriteria criteria);
+}
---
+++ GLB/src/main/java/br/com/sglb/model/entities/Agencia.java
@@ -0,0 +1,197 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.sglb.model.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Israel Araújo
+ */
+@Entity
+@Table(name = "agencia")
+public class Agencia implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 2147483647)
+    @Column(name = "digito")
+    private String digito;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 2147483647)
+    @Column(name = "cep")
+    private String cep;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 2147483647)
+    @Column(name = "logradouro")
+    private String logradouro;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 2147483647)
+    @Column(name = "numero")
+    private String numero;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 2147483647)
+    @Column(name = "descricao")
+    private String descricao;
+    @Column(name = "usuario")
+    private Integer usuario;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "agenciaId")
+    private List<Conta> contaList;
+    @JoinColumn(name = "cidade_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Cidade cidadeId;
+    @JoinColumn(name = "banco_id", referencedColumnName = "cnpj")
+    @ManyToOne(optional = false)
+    private Banco bancoId;
+
+    public Agencia() {
+    }
+
+    public Agencia(Integer id) {
+        this.id = id;
+    }
+
+    public Agencia(Integer id, String digito, String cep, String logradouro, String numero, String descricao) {
+        this.id = id;
+        this.digito = digito;
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDigito() {
+        return digito;
+    }
+
+    public void setDigito(String digito) {
+        this.digito = digito;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Integer usuario) {
+        this.usuario = usuario;
+    }
+
+    @XmlTransient
+    public List<Conta> getContaList() {
+        return contaList;
+    }
+
+    public void setContaList(List<Conta> contaList) {
+        this.contaList = contaList;
+    }
+
+    public Cidade getCidadeId() {
+        return cidadeId;
+    }
+
+    public void setCidadeId(Cidade cidadeId) {
+        this.cidadeId = cidadeId;
+    }
+
+    public Banco getBancoId() {
+        return bancoId;
+    }
+
+    public void setBancoId(Banco bancoId) {
+        this.bancoId = bancoId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Agencia)) {
+            return false;
+        }
+        Agencia other = (Agencia) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.sglb.model.entities.Agencia[ id=" + id + " ]";
+    }
+    
+}
